b55eadd83c8b57d85fd4719ca665b2d129a0bbe6
Desmond Liu <desmondzyliu@gmail.com>
2023-04-24 17:54:05 +0200

    init

---
+++ .vimrc
@@ -0,0 +1,11 @@
+set relativenumber
+set number
+set showcmd
+set smartindent
+
+set tabstop=4
+set shiftwidth=4
+set expandtab
+set softtabstop=2
+
+set nocompatible
---
+++ install.sh
@@ -0,0 +1,13 @@
+# oh my zsh
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+cp .zshrc ~/.zshrc
+
+# conda
+sh -c "$(wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh)"
+conda install -n base conda-libmamba-solver
+conda config --set solver libmamba
+
+# vim
