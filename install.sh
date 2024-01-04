0aee35c02f5de3410d93cf0d3c8e93f2f0617300
Desmond Liu <desmondzyliu@gmail.com>
2024-01-04 12:39:11 +0800

    add lazygit neovim with lazyvim

--- install.sh
+++ install.sh
@@ -1,15 +1,47 @@
 # homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+# bunch of apps (casks)
+brew install --cask \
+	iterm2 \
+	visual-studio-code \
+	miniconda \
+	inkscape \
+	gimp \
+	google-chrome \
+	firefox \
+	wechat \
+	discord \
+	whatsapp \
+	onedrive \
+	dropbox \
+	zotero
+
+brew install \
+	wget \
+	yt-dlp \
+	thefuck \
+	node \
+	lazygit \
+	nvim
+
+# font install via homebrew
+brew tap homebrew/cask-fonts
+brew install font-fira-code-nerd-font
+
 # oh my zsh
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# nvim
+# https://lazyvim.org/installation
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+rm -rf ~/.config/nvim/.git
 
 # powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 cp .zshrc ~/.zshrc
 
 # conda
-sh -c "$(wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh)"
 conda install -n base conda-libmamba-solver
 conda config --set solver libmamba
 
